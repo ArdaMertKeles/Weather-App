79b25a6d56e65cf46d83bc429228452108c51e5f
Arda Mert Keleş <ardakelesmert@gmail.com>
2025-03-17 16:31:48 +0300

    bug fixes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,6 +6,8 @@ export const Map = ({ lat, lon, isDark, currentData }) => {
 
     if (currentData.length === 0) return null;
 
+    const api_key = process.env.REACT_APP_STADIA_API_KEY
+
     const customIcon = new Icon({
         iconUrl: require('.././assets/location-pin.png'),
         iconSize: [32, 32]
@@ -13,7 +15,7 @@ export const Map = ({ lat, lon, isDark, currentData }) => {
 
     return <MapContainer center={[lat, lon]} zoom={13}>
         <TileLayer attribution={isDark ? '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' : ''}
-            url={isDark ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" : "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"} />
+            url={isDark ? `https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${api_key}` : `https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${api_key}`} />
         <Marker position={[lat, lon]} icon={customIcon}>
             <Popup>
                 {currentData.name}
